0fb8ff8a05e32869aeed493ca95fffa4ec260c92
Matt Ingham <matt.g.ingham@gmail.com>
2021-02-13 15:31:03 +0000

    Addition of Palabra class

---
+++ Shared/Palabra.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class Palabra
+    {
+        public int Id { get; set; }
+        public string SpanishWord { get; set; }
+        public string EnglishWord { get; set; }
+    }
+}
